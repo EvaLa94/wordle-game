4d833dcec9b58acffd94ea717b4dfc75d233bcf1
Eva Lateltin <evalateltin@gmail.com>
2024-03-07 20:57:13 +0100

    refactor: from Serializer to ModelSerializer

--- wordle/solver/serializers.py
+++ wordle/solver/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import FiveCharWord
 
 
-class FiveCharWordSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    word = serializers.CharField(max_length=5)
+class FiveCharWordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FiveCharWord
+        fields = '__all__'
